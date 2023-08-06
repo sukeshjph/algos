52c014bf78bc526987b473574e8c5cf052b37790
Sukesh Dash <sukesjh@yahoo.com>
2023-08-06 13:07:18 +0100

    coding

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,55 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function reverse(str) {
+    return str.split('').reduce((accStr, curValue) =>
+        accStr = curValue + accStr
+        , '');
+}
+
+function getKeyValPair(str) {
+    const objKeyValPair = {};
+
+    for (char of str) {
+        if (objKeyValPair[char]) {
+            objKeyValPair[char] = objKeyValPair[char]++;
+        }
+
+        else {
+            objKeyValPair[char] = 1;
+        }
+    }
+    return objKeyValPair;
+
+}
+
+// function palindrome(str) {
+//     if (str.includes(' ')) {
+//         return false;
+//     }
+
+//     if (str[0] !== str[str.length - 1]) {
+//         return false;
+//     }
+
+//     const strObj = getKeyValPair(str);
+//     const revStrObj = getKeyValPair(reverse(str));
+
+//     for (key in strObj) {
+//         if (!Object.keys(revStrObj).includes(key)) {
+//             return false;
+//         }
+
+//         if (revStrObj[key] !== strObj[key]) {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
+
+function palindrome(str) {
+    return reverse(str) === str;
+}
 
 module.exports = palindrome;
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,35 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//     let revStr = '';
+
+//     for (let index = str.length - 1; index >= 0; index--) {
+//         revStr = revStr + str[index];
+//     }
+
+//     return revStr;
+// }
+
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// function reverse(str) {
+//     let revStr = '';
+
+//     for (char of str) {
+//         revStr = char + revStr;
+//     }
+
+//     return revStr;
+// }
+
+function reverse(str) {
+    return str.split('').reduce((accStr, curValue) => {
+        return accStr = curValue + accStr;
+    }, '');
+}
+
 
 module.exports = reverse;
